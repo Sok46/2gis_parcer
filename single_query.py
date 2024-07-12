f4315ba1a7141951ca08c73e8d438ceee2d31d8a
sergey.biryukov <sbryuk0@gmail.com>
2024-07-12 18:44:38 +0300

    Single_query теперь парсит. Нужно дописать QFileDialog

--- single_query.py
+++ single_query.py
@@ -1,8 +1,10 @@
 import sys
-from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QPushButton,QLineEdit,QButtonGroup,QVBoxLayout,QCheckBox)
+import os
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QPushButton,QLineEdit,QButtonGroup,QVBoxLayout,QCheckBox,QFileDialog)
 from PyQt6.QtGui import QFont, QPixmap
 from PyQt6.QtCore import Qt
 from selenium.webdriver.chrome.options import Options
+import datetime
 
 
 import time
@@ -36,7 +38,10 @@ class MainWindow(QWidget):
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=chrome_options)
-        url = input('вставьте ссылку на карту: ')
+        # url = input('вставьте ссылку на карту: ')
+        url = self.link_url.text()
+
+
 
         self.driver.get(url)
 
@@ -46,8 +51,71 @@ class MainWindow(QWidget):
     def prnt(self):
         print('Спарсить')
 
+    def parceElement(self):
+        points = []
+        ulitsa = []
+        geos = []
+        arr_lat = []
+        arr_long = []
+        arr_stars = []
+        arr_voices = []
+        type_arr = []
+        def find_item(by_method, value):
+            if by_method == 'class':
+                item = self.driver.find_element(By.CLASS_NAME, value)
+            elif by_method == 'css':
+                item = self.driver.find_element(By.CSS_SELECTOR, value)
+            try:
+                item = item.text
+            except:
+                item = 'error'
+            return item
+
+
+        name = find_item('class','_tvxwjf' )
+        type_item = find_item('class','_1idnaau')
+        street = find_item('css','#root > div > div > div._1sf34doj > div._1u4plm2 > div:nth-child(2) > div:nth-child(2) > div > div > div > div > div._jcreqo > div._fjltwx > div > div._3zzdxk > div > div > div > div > div._1b96w9b > div:nth-child(2) > div._t0tx82 > div._8sgdp4 > div > div:nth-child(1) > div._49kxlr > div > div:nth-child(2) > div:nth-child(1)')
+        descr = find_item('class','_1p8iqzw')
+        stars = find_item('css','#root > div > div > div._1sf34doj > div._1u4plm2 > div:nth-child(2) > div:nth-child(2) > div > div > div > div > div._jcreqo > div._fjltwx > div > div._3zzdxk > div > div > div > div > div._1tfwnxl > div._146hbp5 > div > div._y10azs')
+        count_voices = find_item('css','#root > div > div > div._1sf34doj > div._1u4plm2 > div:nth-child(2) > div:nth-child(2) > div > div > div > div > div._jcreqo > div._fjltwx > div > div._3zzdxk > div > div > div > div > div._1tfwnxl > div._146hbp5 > div > div._jspzdm')
+
+
+
+        lat_right = str(self.driver.current_url).split('.')[2].split('&')[0].split('?')[0].split('%')[0]
+        # print(lat_right)
+        lat_left = str(self.driver.current_url).split('.')[1][-2:]
+        lat = str(lat_left) + '.' + str(lat_right)
+
+        long_right = str(self.driver.current_url).split('.')[3].split('&')[0].split('?')[0].split('%')[0]
+        long_left = str(self.driver.current_url).split('.')[2][-2:]
+
+        long = str(long_left) + '.' + str(long_right)
+
+        print(lat, long)
+        type_arr.append(type_item)
+        arr_lat.append(lat)
+        arr_long.append(long)
+
+        geos.append(descr)
+        ulitsa.append(street)
+        arr_stars.append(stars)
+        arr_voices.append(count_voices)
+        points.append(name)
+
+        df = pd.DataFrame({'name': points, 'type': type_arr, 'descr': geos, 'ulitsa': ulitsa, 'stars': arr_stars,
+                           'count_voices': arr_voices, 'lat': arr_lat, 'long': arr_long})
+        path = fr'C:\Users\sergey.biryukov\Desktop\Москва генплан конкурс\Общепит/blya.csv'
+        if os.path.isfile(path): # ставить или не ставить headers
+            head = False
+        else:
+            head = True
+        df.to_csv(path, sep=';',
+                  encoding='utf8', index=False,mode='a', header=head)
+
+
 
     def setUpMainWindow(self):
+        self.filename = datetime.datetime.now()
         """Создайте и расположите виджеты в главном окне."""
         header_label = QLabel("2GIS_Parcer by Sergey_Biryukov")
         header_label.setFont(QFont("Arial", 18))
@@ -55,6 +123,12 @@ class MainWindow(QWidget):
         Qt.AlignmentFlag.AlignCenter)
         question_label = QLabel("Выберете действие")
         question_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        # f_dialog = QFileDialog()
+        # f_dialog
+
+
+
 
         # ratings = ["Открыть браузер", "Спарсить элемент"]
         button_group = QButtonGroup(self)
@@ -75,15 +149,22 @@ class MainWindow(QWidget):
         button_group.addButton(self.browser_button)
         self.browser_button.clicked.connect(self.openBrowser)
         main_v_box.addWidget(self.browser_button)
-        main_v_box.addWidget(self.confirm_button)
-        self.setLayout(main_v_box)
+
+        # self.setLayout(main_v_box)
+
+        self.link_url = QLineEdit()
+        main_v_box.addWidget(self.link_url)
 
         self.parce_button = QPushButton("Спарсить элемент")
         button_group.addButton(self.parce_button)
-        self.parce_button.clicked.connect(self.prnt)
+        self.parce_button.clicked.connect(self.parceElement)
+
+
         main_v_box.addWidget(self.parce_button)
         main_v_box.addWidget(self.confirm_button)
         self.setLayout(main_v_box)
+
+
 
 
 
